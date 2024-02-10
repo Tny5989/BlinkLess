0fd5fbb36e15bf4e39259012943bf3341a42b4c4
= <=>
2024-02-10 15:16:12 -0600

    restore gear after switching targets

--- blinkless.lua
+++ blinkless.lua
@@ -6,30 +6,52 @@ require('common')
 
 ------------------------------------------------------------------------------------------------------------------------
 local slots = {
-    Head = { offset = 0x00, func = function(idx, memory)
+    Head = { offset = 0x00, getter = function(idx, memory)
         return memory:GetEntity():GetLookHead(idx)
+    end, setter = function(idx, gear, memory)
+        memory:GetEntity():SetLookHead(idx, gear)
     end },
-    Body = { offset = 0x02, func = function(idx, memory)
+    Body = { offset = 0x02, getter = function(idx, memory)
         return memory:GetEntity():GetLookBody(idx)
+    end, setter = function(idx, gear, memory)
+        memory:GetEntity():SetLookBody(idx, gear)
     end },
-    Hands = { offset = 0x04, func = function(idx, memory)
+    Hands = { offset = 0x04, getter = function(idx, memory)
         return memory:GetEntity():GetLookHands(idx)
+    end, setter = function(idx, gear, memory)
+        memory:GetEntity():SetLookHands(idx, gear)
     end },
-    Legs = { offset = 0x06, func = function(idx, memory)
+    Legs = { offset = 0x06, getter = function(idx, memory)
         return memory:GetEntity():GetLookLegs(idx)
+    end, setter = function(idx, gear, memory)
+        memory:GetEntity():SetLookLegs(idx, gear)
     end },
-    Feet = { offset = 0x08, func = function(idx, memory)
+    Feet = { offset = 0x08, getter = function(idx, memory)
         return memory:GetEntity():GetLookFeet(idx)
+    end, setter = function(idx, gear, memory)
+        memory:GetEntity():SetLookFeet(idx, gear)
     end },
-    Main = { offset = 0x0A, func = function(idx, memory)
+    Main = { offset = 0x0A, getter = function(idx, memory)
         return memory:GetEntity():GetLookMain(idx)
+    end, setter = function(idx, gear, memory)
+        memory:GetEntity():SetLookMain(idx, gear)
     end },
-    Sub = { offset = 0x0C, func = function(idx, memory)
+    Sub = { offset = 0x0C, getter = function(idx, memory)
         return memory:GetEntity():GetLookSub(idx)
+    end, setter = function(idx, gear, memory)
+        memory:GetEntity():SetLookSub(idx, gear)
     end },
-    Ranged = { offset = 0x0E, func = function(idx, memory)
+    Ranged = { offset = 0x0E, getter = function(idx, memory)
         return memory:GetEntity():GetLookRanged(idx)
+    end, setter = function(idx, gear, memory)
+        memory:GetEntity():SetLookRanged(idx, gear)
     end },
+}
+
+------------------------------------------------------------------------------------------------------------------------
+local cache = {
+    target_idx = 0,
+    sub_target_idx = 0,
 }
 
 ------------------------------------------------------------------------------------------------------------------------
@@ -50,11 +72,18 @@ end
 ------------------------------------------------------------------------------------------------------------------------
 local function GetPatchedValue(data, player_idx, slot, offset, memory)
     local packet_gear = struct.unpack('H', data, offset + slot.offset)
-    local memory_gear = slot.func(player_idx, memory)
+    local memory_gear = slot.getter(player_idx, memory)
+
+    if (cache[player_idx] == nil) then
+        cache[player_idx] = {}
+    end
 
     if (memory_gear == 0) then
+        cache[player_idx][slot.offset] = nil
         return packet_gear
     end
+
+    cache[player_idx][slot.offset] = packet_gear
 
     return memory_gear
 end
@@ -103,6 +132,30 @@ local function PatchPacket(e, player_idx, memory)
 end
 
 ------------------------------------------------------------------------------------------------------------------------
+local function RestoreGear(player_idx, memory)
+    if (player_idx == 0 or cache[player_idx] == nil) then
+        return
+    end
+
+    local update = false
+    for _, value in pairs(slots) do
+        local gear = cache[player_idx][value.offset]
+        if (gear ~= nil) then
+            local current_gear = value.getter(player_idx, memory)
+            if (current_gear ~= gear) then
+                update = true
+                value.setter(player_idx, gear, memory)
+            end
+        end
+    end
+    cache[player_idx] = nil
+
+    if (update) then
+        memory:GetEntity():SetModelUpdateFlags(player_idx, bit.bor(memory:GetEntity():GetModelUpdateFlags(player_idx), 0x10))
+    end
+end
+
+------------------------------------------------------------------------------------------------------------------------
 ashita.events.register('packet_in', 'bl_packet_in_cb', function(e)
     if (e.id == 0x00D) then
         local main_target_idx = GetTarget(0)
@@ -130,5 +183,24 @@ ashita.events.register('packet_in', 'bl_packet_in_cb', function(e)
         end
 
         PatchPacket(e, self_idx, AshitaCore:GetMemoryManager())
+    elseif (e.id == 0x00A or e.id == 0x00B) then
+        cache = { target_idx = 0, sub_target_idx = 0 }
     end
+end)
+
+------------------------------------------------------------------------------------------------------------------------
+ashita.events.register('d3d_present', 'bl_present_cb', function()
+    local target_idx = GetTarget(0)
+    local sub_target_idx = GetTarget(1)
+    local memory = AshitaCore:GetMemoryManager()
+
+    if (cache.target_idx ~= target_idx and cache.target_idx ~= sub_target_idx) then
+        RestoreGear(cache.target_idx, memory)
+    end
+    if (cache.sub_target_idx ~= target_idx and cache.sub_target_idx ~= sub_target_idx) then
+        RestoreGear(cache.sub_target_idx, memory)
+    end
+
+    cache.target_idx = target_idx
+    cache.sub_target_idx = sub_target_idx
 end)
